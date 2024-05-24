935e8e184bec41baaf3758176cf617e6e33e92c8
Anand Verma <anandverma552@gmail.com>
2024-05-24 17:23:21 -0400

    html webpack plugin added

--- introduction/webpack.config.js
+++ introduction/webpack.config.js
@@ -2,12 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath: '',
   },
   mode: "none",
   module: {
@@ -53,5 +55,12 @@ module.exports = {
             path.join(process.cwd(), 'build/**/*')
         ]
     }),
+    new HtmlWebpackPlugin({
+        title: 'Hello World',
+        filename: 'customIndex.html',
+        meta: {
+            description: 'Some Description'
+        }
+    })
   ],
 };
