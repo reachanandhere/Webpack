ed4730742b23eb947ad740c0edf7698fd1bed5b3
Anand Verma <anandverma552@gmail.com>
2024-05-24 18:16:37 -0400

    Webpack dev server added

--- introduction/webpack.development.config.js
+++ introduction/webpack.development.config.js
@@ -11,6 +11,15 @@ module.exports = {
     publicPath: '',
   },
   mode: "development",
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, "./dist")
+    },
+    devMiddleware: {
+      index: 'index.html'
+    }
+  },
   module: {
     rules: [
       {
